465825e6876d940417c8b39813e8d8448576e79e
MatiasAlvadePierola <matiasdesuyo@gmail.com>
2022-07-31 18:06:22 +0200

    Header v.0.2

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,7 @@
 import logo from '../img/logo.png';
 import '../css/App.css';
+import cuenta from '../img/cuenta.png';
+
 
 function App() {
   return (
@@ -7,11 +9,15 @@ function App() {
       <div className='header-top'>
         <div className='header-business'>
             <img className='header-logo' src={logo} alt="logo"/>
+            <img className='cuenta' src={cuenta} alt="cuenta"/>
+
             <span className='header-title'>BunWe</span>
         </div>
       </div>
       <div className='header-bottom'>
-
+      <a className="sub" href="" style={{marginLeft: '25%'}}>Chats</a>
+      <a className="sub" href="">Mundo</a>
+      <a className="sub" href="">Noticias</a>
       </div>
     </div>
   );
