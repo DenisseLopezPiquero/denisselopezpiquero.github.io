552a095e4457883fc301fd5623e375c336f0e665
MatiasAlvadePierola <matiasdesuyo@gmail.com>
2022-07-31 20:30:24 +0200

    headet-top v0.5

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,6 +2,9 @@ import logo from '../img/logo.png';
 import headerBell from '../img/bell.png';
 import '../css/App.css';
 import cuenta from '../img/cuenta.png';
+import lupa from '../img/lupa.png';
+import españa from '../img/españa.png';
+import options from '../img/options.png';
 
 
 function App() {
@@ -10,20 +13,23 @@ function App() {
       <div className='header-top'>
         <div className='header-business'>
             <img className='header-logo' src={logo} alt="logo"/>
-            <img className='cuenta' src={cuenta} alt="cuenta"/>
-
             <span className='header-title'>BunWe</span>
         </div>
         <img className='header-bell' src={headerBell} alt='Notifications'/>
-        <div>
+        <div style={{display: "inline-flex"}}>
           <input className='header-search' placeholder='Buscar...'></input>
+          <button className='header-button'><img className='header-button-img' src={lupa} alt="search"/></button>
         </div>
+        <div className='header-new-post'><span className='header-text-np'>+</span></div>
+        <div className='header-lang'><img className='header-lang-img' src={españa} alt='lang'/></div>
+        <img className='header-options' src={options} alt='lang'/>
       </div>
       <div className='header-bottom'>
-      <a className="sub" href="" style={{marginLeft: '25%'}}>Chats</a>
-      <a className="sub" href="">Mundo</a>
-      <a className="sub" href="">Noticias</a>
+        <a className="sub" href="www.google.com" style={{marginLeft: '25%'}}>Chats</a>
+        <a className="sub" href="www.google.com">Mundo</a>
+        <a className="sub" href="www.google.com">Noticias</a>
       </div>
+      <img className='cuenta' src={cuenta} alt="cuenta"/>
     </div>
   );
 }
