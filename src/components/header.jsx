593f295946d54e7baf95b332055ed22b164db2f8
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2022-08-02 18:04:31 +0200

    make it better

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from '../img/logo.gif';
 import headerBell from '../img/bell.png';
 import '../css/App.css';
@@ -11,36 +12,42 @@ import carrito from '../img/carrito.png';
 function App() {
   return (
     <div className="App">
-      <div className='header-top'> 
-        <div className='header-divisor'>
-          <div className='header-business'>
-              <img className='header-logo' src={logo} alt="logo"/>
+      <div id='header'>
+        <div className='header-top'> 
+          <div className='header-divisor'>
+            <div className='header-business'>
+                <img className='header-logo' src={logo} alt="logo"/>
+            </div>
+            <img className='header-bell' src={headerBell} alt='Notifications'/>
           </div>
-          <img className='header-bell' src={headerBell} alt='Notifications'/>
-        </div>
-        <div className='header-divisor'>
-          <div className='header-central'>
-            <input className='header-search' placeholder='Buscar...'></input>
-            <button className='header-button'><img className='header-button-img' src={lupa} alt="search"/></button>
+          <div className='header-divisor'>
+            <div className='header-central'>
+              <input className='header-search' placeholder='Buscar...'></input>
+              <button className='header-button'><img className='header-button-img' src={lupa} alt="search"/></button>
+            </div>
+            <div className='header-new-post'><span className='header-text-np'>+</span></div>
           </div>
-          <div className='header-new-post'><span className='header-text-np'>+</span></div>
+          <div className='header-divisor'>
+            <div className='header-lang'><img className='header-lang-img' src={españa} alt='lang'/></div>
+            <img className='header-options' src={options} alt='lang'/>
+          </div>
         </div>
-        <div className='header-divisor'>
-          <div className='header-lang'><img className='header-lang-img' src={españa} alt='lang'/></div>
-          <img className='header-options' src={options} alt='lang'/>
+        <div className='header-bottom'>
+          <div className='header-sub-bottom'>
+            <div className="sub"style={{marginLeft: '16.6666%'}}>Chats</div>
+            <div className="sub">Directos</div>
+            <div className="sub">Mundo</div>
+            <div className="sub">Amigos</div>
+            <div className="sub">Noticias</div>
+          </div>
+          <img className='cuenta' src={cuenta} alt="cuenta"/>
+          <div>
+            <img className='carrito' src={carrito} alt="carrito"/>
+          </div>
         </div>
       </div>
-      <div className='header-bottom'>
-        <div className='header-sub-bottom'>
-          <div className="sub" href="www.google.com" style={{marginLeft: '25%'}}>Chats</div>
-          <div className="sub" href="www.google.com">Mundo</div>
-          <div className="sub" href="www.google.com">Noticias</div>
-        </div>
-        <img className='cuenta' src={cuenta} alt="cuenta"/>
-        <div>
-          <img className='carrito' src={carrito} alt="carrito"/>
-        </div>
-
+      <div id='home'>
+        
       </div>
     </div>
     
