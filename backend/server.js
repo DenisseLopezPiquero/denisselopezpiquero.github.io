dbd2defb2f4ef7af395507ce5a0d832abc1303b9
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2022-08-09 16:33:49 +0200

    Deleting node_modules

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const Users = require('./api/users');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api/users', Users);
@@ -15,7 +15,7 @@ mongoose.connect('mongodb://localhost/users',
     (err, res) => { 
         err && console.log('Error conectando a la bd');
         app.listen(4000, () => {
-            console.log('Server running on port 4000');
+            console.log('Server corriendo en el puerto 4000');
         });
     }
 );
---
+++ src/components/Perfil.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Perfil extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Perfil</h1>
+      </div>
+    );
+  } 
+}
