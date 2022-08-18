7a922adec0f50520786b2a080188bab82151022e
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2022-08-18 18:56:03 +0200

    Home responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import '../css/App.css';
 import Home from './Home.jsx';
 import Auth from './Auth.jsx';
@@ -11,20 +11,23 @@ import españa from '../img/españa.png';
 import options from '../img/options.png';
 import cuenta from '../img/cuenta.jpg';
 import carrito from '../img/carrito.png';
+import OutsideAlerter from "./OutsideAlerter";
 
 
 function App() {
 	var [logged, setLogged] = useState(true);
+	var [optionclick, setOptionClick] = useState(false);
 	var mainLogged = event => {setLogged(current => false);};
+	var clickOptions = event => {setOptionClick(current => !current);};
 	return (
 		<Router>
 			<div className="App">
 				{logged ? <div id='header'>
 					<div className='header-top'>
             			<div className='header-divisor'>
-            			  <div className='header-business'>
+            			  <Link to='home'><div className='header-business'>
             			      <img className='header-logo' src={logo} alt="logo"/>
-            			  </div>
+            			  </div></Link>
             			  <img className='header-bell' src={headerBell} alt='Notifications'/>
             			</div>
             			<div className='header-divisor'>
@@ -36,8 +39,20 @@ function App() {
             			</div>
             			<div className='header-divisor'>
             			  <div className='header-lang'><img className='header-lang-img' src={españa} alt='lang'/></div>
-            			  <img className='header-options' src={options} alt='lang'/>
+						  <div className='header-options-container'>
+            			  	<img id='options_button' onClick={clickOptions} className='header-options' src={options} alt='option'/>
+							{optionclick ? <OutsideAlerter setisActive={clickOptions} isActive={optionclick} parameterid='options_button'>
+								<div className='header-options-div'>
+								<Link className='link-options-hover' to="/">
+									<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+									  <path stroke="none" d="M0 0h24v24H0z"/>
+									  <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+									  <path d="M7 12h14l-3 -3m0 6l3 -3" />
+									</svg><span className='header-logout-button'>Cerrar sesión</span></Link>
+							</div></OutsideAlerter>: null}
+						  </div>
             			</div>
+						
           			</div>
 		  			<div className='header-bottom'>
           				<div className='header-sub-bottom'>
@@ -47,13 +62,13 @@ function App() {
           				  	<div className="header-sub">Amigos</div>
           				  	<div className="header-sub">Noticias</div>
           				</div>
-          				<img className='header-cuenta' src={cuenta} alt="cuenta"/>
+          				<Link className='header-cuenta' to="profile"><img className='header-cuenta-img' src={cuenta} alt="cuenta"/></Link>
           				<div>
           				  <img className='header-carrito' src={carrito} alt="carrito"/>
           				</div>
         			</div>
 				</div>
-				: <div></div>}
+				: null}
 				<Routes>
 					<Route exact path="/" element={<Auth setisLogged={mainLogged} isLogged={logged} />} />
 					<Route path="/home" element={<Home />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,166 @@
 import React from "react";
+import '../css/Home.css'
 
 export default class Home extends React.Component {
 	render() {
+		const ViewPics = [
+			{
+				user: "user1",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			{
+				user: "user2",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			{
+				user: "user3",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			{
+				user: "user4",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			{
+				user: "user5",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			{
+				user: "user6",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			{
+				user: "user7",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			{
+				user: "user8",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			{
+				user: "user9",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			{
+				user: "user10",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+			},
+			
+		];
+		const posts = [
+			{
+				user: "user1",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+				caption: "This is a caption",
+				body: "This is a bodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybody",
+				likes: "10",
+				comment: "2",
+				shares: "1",
+				date: "1 day ago",
+				comments: [
+					{
+						user: "user1",
+						comment: "This is a comment",
+						date: "1 day ago",
+					},
+				]
+			},
+			{
+				user: "user2",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+				caption: "This is a caption",
+				body: "This is a bodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybody",
+				likes: "10",
+				comment: "2",
+				shares: "1",
+				date: "1 day ago",
+				comments: [
+					{
+						user: "user1",
+						comment: "This is a comment",
+						date: "1 day ago",
+					},
+				]
+			},
+			{
+				user: "user3",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+				caption: "This is a caption",
+				body: "This is a bodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybody",
+				likes: "10",
+				comment: "2",
+				shares: "1",
+				date: "1 day ago",
+				comments: [
+					{
+						user: "user1",
+						comment: "This is a comment",
+						date: "1 day ago",
+					},
+				]
+			},
+			{
+				user: "user4",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+				caption: "This is a caption",
+				body: "This is a bodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybody",
+				likes: "10",
+				comment: "2",
+				shares: "1",
+				date: "1 day ago",
+				comments: [
+					{
+						user: "user1",
+						comment: "This is a comment",
+						date: "1 day ago",
+					},
+				]
+			},
+			{
+				user: "user5",
+				src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+				caption: "This is a caption",
+				body: "This is a bodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybody",
+				likes: "10",
+				comment: "2",
+				shares: "1",
+				date: "1 day ago",
+				comments: [
+					{
+						user: "user1",
+						comment: "This is a comment",
+						date: "1 day ago",
+					},
+				]
+			},
+		]
 		return (
-		<div>
-			<h1>Home</h1>
+		<div className="home-container">
+			<div className="home-main-container">
+				<div className="home-posts-container">
+					{posts.map((post) => 
+					<div className="home-each-post-container">
+						<div className="home-avatar-post">
+							<div className="home-topwing-post"></div>
+							<div className="home-avatar-background">
+								<img className="home-avatar-img" src={post.src} alt="avatar" />
+							</div>
+							<div className="home-bottomwing-post"></div>
+						</div>
+					</div>
+					)}
+				</div>
+				<div className="home-viewpic-container">
+					{ViewPics.map((pic) => 
+						<div>
+							<img className="home-viepic-picture" src={pic.src} alt="" />
+						</div>
+					)}
+				</div>
+			</div>
+			<div className="home-divisor-containers"></div>
+			<div className="home-second-container">
+
+			</div>
 		</div>
 		);
 	}
