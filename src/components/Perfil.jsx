d5e1f0675abf0f2816db5fe952c3d1b6ee276591
MatiasAlvadePierola <matiasdesuyo@gmail.com>
2022-08-23 14:19:29 +0200

    awa

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import '../css/Profile.css';
+import fotoprofile from '../img/cuenta.jpg';
+
 
 
 export default class Perfil extends React.Component {
 	render() {
 		return (
 		<div>
+			<div>
+			   <span className="profile1">
+				FGRG
+			   </span>
+			</div>
+			<div>
+			<center>
+			<img className='fotoprofile' src={fotoprofile} alt='picture'/>
+			</center>
+			<div className='Username'>
+				<span>
+					Username
+				</span>
+				</div>
+				</div>
 			<h1>Perfil</h1>
+			<div className='level'>
+			<span>
+			 Level
+			</span>
+			</div>
 		</div>
 		);
 	}
