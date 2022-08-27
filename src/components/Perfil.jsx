bdc34ea03997d27b6de24597eae7a30d0c0cdb9f
davidsantapau <davidallozaisantapau@gmail.com>
2022-08-27 11:21:12 +0200

    swfewegrdr

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import '../css/Perfil.css'
+import cuenta from '../img/cuenta.jpg';
 
 export default class Perfil extends React.Component {
 	render() {
 		return (
 		<div>
-			<h1>Perfil</h1>
+			<img className='image' src={cuenta} alt="cuenta" />
+			<h1 className='header'>nickname_name</h1>
 		</div>
 		);
 	}
